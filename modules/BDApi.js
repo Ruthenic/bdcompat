39c9523e289efe098886b20b48b7eedcf6056720
Ruthenic <mdrakea3@tutanota.com>
2021-11-04 21:00:40 -0400

    Start work on custom patcher

--- modules/BDApi.js
+++ modules/BDApi.js
@@ -18,6 +18,25 @@ let settings = {
 	}
 }
 
+//This is a patcher that will be used to patch target functions, utilizing a Proxy object to run the specified function after the original function has been called, before the original function has been called, and replacing the function entirely
+let Patcher = {
+	//This is the function that will be used to patch a target function
+	//targetFunction: the function to be patched (string)
+	//targetParent: the object that contains the targetFunction (object)
+	//patchFunction: the function to be called after the target function has been called if defined (function or undefined)
+	//beforeFunction: the function to be called before the target function has been called if defined (function or undefined)
+	//replaceFunction: the function to be called instead of the target function if defined (function or undefined)
+	patch: (targetFunction, targetParent, patchFunction, beforeFunction, replaceFunction) => {
+		//If the target function is not defined, return
+		if(!targetFunction) return;
+		//If the target function is not a string, return
+		if(typeof targetFunction !== 'string') return;
+		
+		//Create a proxy on the targetParent object
+		let newProxy = new Proxy();
+	}
+}
+
 let BdApi = {
 	React: commonModules.React,
 	ReactDOM: commonModules.ReactDOM,
@@ -28,7 +47,7 @@ let BdApi = {
 	screenWidth: screen.width,
 	screenHeight: screen.height,
 	alert: (title, content) => {
-		console.log(`BDCompat Toast\nTitle: ${title}\nContent:${content}`)
+		window.alert(`BDCompat Toast\nTitle: ${title}\nContent:${content}`)
 	},
 	clearCSS: (id) => {
 		//stub because i don't care about themes
